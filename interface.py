d52e4cbe62a46782ec0be6c4e54ff3087965c633
Alex/t3 <104983707+burger4d@users.noreply.github.com>
2025-01-10 18:48:08 +0100

    Update interface.py

--- interface.py
+++ interface.py
@@ -4,5 +4,15 @@ tk = Tk()
 tk.title("Lenia")
 #tk.resizable(False, False)
 tk.configure(cursor = "hand2")
-w = Canvas(tk,width=640,height=640,bg="#"+str(hex(255))[2:]+str(hex(255)[2:]+str(hex(255))[2:]))
+taille_canevas = 500
+w = Canvas(tk,width=taille_canevas,height=taille_canevas,bg="white")
 w.pack()
+
+def show_mat(A):
+    w.itemconfig("tile", state='hidden')
+    unite = int(taille_canevas//len(A))
+    for i in range(len(A)):
+        for j in range(len(A[0])):
+            if A[i][j] == 1:
+                w.create_rectangle(j*unite, i*unite, (j+1)*unite, (i+1)*unite, fill="black", tag="tile")
+    tk.update()
