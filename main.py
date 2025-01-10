4a779f54c761a1ee6ab2df3931593980830f8a1b
Alex/t3 <104983707+burger4d@users.noreply.github.com>
2025-01-10 18:47:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,43 @@
-from tkinter import *
 import numpy as np
 from scipy import signal
-A = np.array([[2,1,3,0],[1,1,0,5],[3,3,1,0],[2,0,0,2]])
-M = np.array([[1,0,2],[2,1,0],[1,0,3]])
-B = signal.convolve2d(A, M, mode='same', boundary='fill')
+import random
+from interface import *
 
-'''
-tk = Tk()
-tk.title("Lenia")
-#tk.resizable(False, False)
-tk.configure(cursor = "hand2")
-w = Canvas(tk,width=640,height=640,bg="#"+str(hex(255))[2:]+str(hex(255)[2:]+str(hex(255))[2:]))
-w.pack()
-'''
+taille = 10
 
-            
-def convol(mat, kernel):
-    n = len(mat)
-    mat2 = [n*[0] for i in range(n)]
-    for i in range(len(mat)):
-        for j in range(len(mat)):
-            acc = 0
-            for x in range(len(kernel)):
-                for y in range(len(kernel)):
-                    if x==i and y==j:
-                        acc+=mat[i][j]*kernel[x][y]
-            mat2[i][j] = acc
-    return mat2
+#A=[[random.randint(0,1) for i in range(taille)] for j in range(taille)]
+A = [[0 for i in range(taille)] for j in range(taille)]
+A[0][1] = 1
+A[1][2] = 1
+A[2][0] = 1
+A[2][1] = 1
+A[2][2] = 1
+def print_mat(A):
+    for i in A:
+        print(i)
+
+def update(A):
+    B = [[0 for i in range(taille)] for j in range(taille)]
+    for i in range(taille):
+        for j in range(taille):
+            voisins = 0
+            for x in range(-1, 2):
+                for y in range(-1, 2):
+                    if (x, y) != (0, 0):
+                        voisins+=A[(i+x+taille)%taille][(j+y+taille)%taille]
+            #Une cellule morte possédant exactement trois cellules voisines vivantes devient vivante (elle naît) ;
+            #Une cellule vivante ne possédant pas exactement deux ou trois cellules voisines vivantes meurt.
+            B[i][j] = A[i][j]
+            if i==0 and j==1:
+                print(voisins)
+            if A[i][j] == 0 and voisins==3:
+                B[i][j] = 1
+            elif A[i][j] == 1 and (voisins>3 or voisins<2):
+                B[i][j] = 0
+    return B
+
+while 1:
+    print_mat(A)
+    show_mat(A)
+    input()
+    A=update(A)
